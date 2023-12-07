44c9c9aa46902ad4d0ea68453f0964be41104074
Rycerzklanu <szymkris5@gmail.com>
2023-12-07 18:39:12 +0100

    BST work start

--- main.c
+++ main.c
@@ -2,6 +2,47 @@
 #include<string.h>
 #include<stdio.h>
 
+//drzewo binarne, pozwalające na efektywne "podróżowanie" po strukturze gry
+//krok 1. deklaracja struktury drzewa, pozwala ona na podróż także w górę drzewa
+struct TreeNode{
+int indentifier;
+char message[500];
+struct TreeNode* right;
+struct TreeNode* left;
+struct TreeNode* parent;
+};
+
+//krok 2. funkcja tworząca nowe elementy drzewa
+struct TreeNode* createNode(int identifier, char message[500])
+{
+    struct TreeNode* newTreeNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newTreeNode->identifier = identifier;
+    strcpy(newTreeNode->message, message);
+    newTreeNode->left = newTreeNode->right = newTreeNode->parent = NULL;
+    return newTreeNode;
+}
+
+//krok 3. funkcja dodająca element do drzewa po utworzeniu
+//sprawdza ona, czy dany element jest pierwszym (root)
+//jeżeli tak, to tworzony jest korzeń (root)
+//jeżeli nie, tworzony jest kolejny element drzewa
+struct TreeNode* insertNode(struct TreeNode* root, int identifier, char message, struct TreeNode* parent)
+{
+    if (root == NULL)
+    {
+        struct TreeNode* newTreeNode = createNode(identifier. message);
+        newTreeNode->parent = parent;
+        return newTreeNode;
+    }
+
+    if (strcmp(identifier, root->indentifier) < 0){
+        root->left = In
+    }
+}
+
+
+//sekcja kodu odpowiedzialna za odpowiedzi gracza i ich walidację
+//implementacja tych funkcji znajduje się pod main() dla lepszej czytelności kodu
 int InputReader();
 int InputValidation();
 
